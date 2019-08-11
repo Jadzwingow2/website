04aa3f5e64d51d883f841b90c4d15f7a70453823
Jarek Wasowski <jarek@truesoftware.pl>
2019-08-11 14:36:07 +0200

    update

---
+++ precache-manifest.991eb33e715d5f6ba6d603fd02a18acb.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "dd4454a73c47b9983f9b7fa9416d5938",
+    "url": "/ms-icon-310x310.png"
+  },
+  {
+    "revision": "73bdca226fdd2e01ff8994d86b431440",
+    "url": "/ms-icon-70x70.png"
+  },
+  {
+    "revision": "eddff6f3b8a8d1eacadf58dd4e2c8d7d",
+    "url": "/favicon-96x96.png"
+  },
+  {
+    "revision": "077d2a7466ce849af0ac",
+    "url": "/js/about.438f1f7a.js"
+  },
+  {
+    "revision": "c25633536edc98ff44beac7d854f924d",
+    "url": "/ms-icon-144x144.png"
+  },
+  {
+    "revision": "8e6d22859f82817fa3a2efdcc3bc602a",
+    "url": "/ms-icon-150x150.png"
+  },
+  {
+    "revision": "8dbb06ba5a22c5d3dc74",
+    "url": "/js/app.5919a3e6.js"
+  },
+  {
+    "revision": "be59afcde25a340a8879",
+    "url": "/js/chunk-vendors.75adf070.js"
+  },
+  {
+    "revision": "c25633536edc98ff44beac7d854f924d",
+    "url": "/android-icon-144x144.png"
+  },
+  {
+    "revision": "be59afcde25a340a8879",
+    "url": "/css/chunk-vendors.ac52170f.css"
+  },
+  {
+    "revision": "73ad51cbb2c91fdaa0ca52e5c86e315a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7809e7e6de5200c275fce6cb772a5a71",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "53c41449885431187cd5e6c60fd91d6c",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "efdfc13eae202c6cd3eeee14b439e15f",
+    "url": "/apple-icon-114x114.png"
+  },
+  {
+    "revision": "682cdfb16c2ad783c66190a43ed98f7b",
+    "url": "/apple-icon-60x60.png"
+  },
+  {
+    "revision": "8dbb06ba5a22c5d3dc74",
+    "url": "/css/app.c48a7704.css"
+  },
+  {
+    "revision": "a0d5f0f08155fba2aad2b86af4705ac4",
+    "url": "/apple-icon-76x76.png"
+  },
+  {
+    "revision": "b6ae93c26ee2824ffc943729aaac8fc0",
+    "url": "/apple-icon-72x72.png"
+  },
+  {
+    "revision": "de0620503fc3888948289432ec885559",
+    "url": "/apple-icon.png"
+  },
+  {
+    "revision": "de0620503fc3888948289432ec885559",
+    "url": "/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "3336dd58de6c963e16687c0806ec1d3b",
+    "url": "/android-icon-192x192.png"
+  },
+  {
+    "revision": "eb1258152dc60c34030dc5ee21e46103",
+    "url": "/apple-icon-57x57.png"
+  },
+  {
+    "revision": "e9bfc5dbec6462f685e067cad0bab283",
+    "url": "/apple-icon-180x180.png"
+  },
+  {
+    "revision": "fe0dac5320a2db5dcf18f3c1b3f7f49b",
+    "url": "/apple-icon-152x152.png"
+  },
+  {
+    "revision": "c25633536edc98ff44beac7d854f924d",
+    "url": "/apple-icon-144x144.png"
+  },
+  {
+    "revision": "1cdc287905c5c6596d795143dff71925",
+    "url": "/apple-icon-120x120.png"
+  },
+  {
+    "revision": "eddff6f3b8a8d1eacadf58dd4e2c8d7d",
+    "url": "/android-icon-96x96.png"
+  },
+  {
+    "revision": "b6ae93c26ee2824ffc943729aaac8fc0",
+    "url": "/android-icon-72x72.png"
+  },
+  {
+    "revision": "233cf5c9564758c1a05ffeba29ac017c",
+    "url": "/android-icon-48x48.png"
+  },
+  {
+    "revision": "69770fc270078c847e0cf447584b39a0",
+    "url": "/android-icon-36x36.png"
+  },
+  {
+    "revision": "e871a616396e45bd7fd1cc0f126b3366",
+    "url": "/README.md"
+  },
+  {
+    "revision": "d2456d87759d039659e42ca37a57973a",
+    "url": "/CNAME"
+  }
+];
